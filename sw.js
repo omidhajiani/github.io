38fc0c100528bc6f43fbd23f5f827978a6da5924
omidhajiani <omidhajiani@gmail.com>
2025-10-24 21:53:50 +0330

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,14 @@
+// This is a very basic service worker.
+self.addEventListener('install', event => {
+  console.log('Service worker installing...');
+  // Add a call to skipWaiting here
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  console.log('Service worker activating...');
+});
+
+self.addEventListener('fetch', event => {
+  console.log('Fetching:', event.request.url);
+});
